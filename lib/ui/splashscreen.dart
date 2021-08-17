7beffccdecf5622bc28129dd252244df0b523165
mosfeqanik <mosfeqanik01@gmail.com>
2021-08-18 00:21:47 +0600

    splashscreen Done

---
+++ lib/ui/splashscreen.dart
@@ -0,0 +1,41 @@
+import 'package:abc/const/appcolors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.deep_green,
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                'E-Commerce',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 44.sp,
+                ),
+              ),
+              SizedBox(
+                height: 15.h,
+              ),
+              CircularProgressIndicator(
+                color: Colors.white,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
